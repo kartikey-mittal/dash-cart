0c985169a89ebf8ce3d52ef81ec298389bfe3d20
Aviral <aviral.saxena.11.70@gmail.com>
2023-10-26 23:24:09 +0530

    LoginScreen.js

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-import { StatusBar } from 'react-native';
+import LoginScreen from './LoginScreen';
+
 
 const TestScreen = () => {
     return (
         <View>
-            <StatusBar backgroundColor="#0a5098" barStyle="light-content" />
-
-            <Text style={{ alignSelf: 'center', fontSize: 30, fontWeight: 500, color: 'orange' }}>TEST SCREEN</Text>
+            
+            <LoginScreen/>
         </View>
     );
 }
