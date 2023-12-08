81740136dcea01310ed97d2678e722c5d8e19139
kartikey-mittal <g6.kartikey@gmail.com>
2023-12-09 02:14:39 +0530

    Side menu rest work

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,6 +1,7 @@
-import React, { useContext, useRef, useEffect } from 'react';
-import { View, Text, Animated, Dimensions, TouchableWithoutFeedback, PanResponder, Alert } from 'react-native';
+import React, { useContext, useRef, useEffect, } from 'react';
+import { View, Text, Animated, Dimensions, TouchableWithoutFeedback, PanResponder, Alert, TouchableOpacity } from 'react-native';
 import { MenuContext } from './MenuContext';
+import Icon from 'react-native-vector-icons/Ionicons'; // replace with your actual icon component
 
 const window = Dimensions.get('window');
 
@@ -42,7 +43,7 @@ const SideMenu = () => {
 
   const handleTextClick = (text) => {
     Alert.alert(`You clicked on ${text}`);
-    if (text === 'Home') {
+    if (text === 'Back') {
       Animated.timing(animateSideMenu, {
         toValue: -window.width,
         duration: 500,
@@ -53,14 +54,30 @@ const SideMenu = () => {
 
   return (
     <View style={{ position: 'absolute', left: 0, top: 0, bottom: 0, right: 0 }}>
-      <Animated.View style={{ position: 'absolute', left: animateSideMenu, top: 0, bottom: 0, width: window.width * 0.7, backgroundColor: 'yellow', padding: 20, zIndex: 1, shadowColor: "#000", shadowOffset: { width: -10, height: 0 }, shadowOpacity: 0.58, shadowRadius: 16.00, elevation: 24 }} {...panResponder.panHandlers}>
-        <TouchableWithoutFeedback onPress={() => {}}>
+      <Animated.View style={{ position: 'absolute', left: animateSideMenu, top: 0, bottom: 0, width: window.width * 0.7, backgroundColor: 'yellow', padding: 0, zIndex: 1, shadowColor: "#000", shadowOffset: { width: -10, height: 0 }, shadowOpacity: 0.58, shadowRadius: 16.00, elevation: 24 }} {...panResponder.panHandlers}>
+        <TouchableWithoutFeedback onPress={() => { }}>
           <View>
-            <Text style={{ marginBottom: 10, fontSize: 18,backgroundColor:'red' }} onPress={() => handleTextClick('Home')}>Home</Text>
-            <View style={{backgroundColor:'orange'}}>
-            <Text style={{ marginBottom: 10, fontSize: 18 }} onPress={() => handleTextClick('Profile')}>Profile</Text>
+            <View style={{ backgroundColor: '#EB8633' }}>
+              <TouchableOpacity style={{ backgroundColor: '#fff', borderRadius: 50, padding: 10, margin: 5, width: 50 }} onPress={() => handleTextClick('Back')}>
+                <Icon name="chevron-back" size={25} color="#EB8633" />
+              </TouchableOpacity>
             </View>
+            <View style={{ backgroundColor: '#EB8633', flexGrow: 1 }}>
+              <Text style={{ color: 'white', fontSize: 20, fontWeight: 300, marginLeft: 5, marginTop: 15 }}>Welcome</Text>
+              <Text style={{ color: 'white', fontSize: 25, fontWeight: 400, marginLeft: 5, elevation: 5 }}>Kartikey</Text>
+            </View>
+
+
+            {/*---------------------SIDE MENU WORK SHOULD BE DONE ⬇️⬇️⬇️⬇️⬇️ ---------------------------------- */}
+            
+            <Text style={{ marginBottom: 10, fontSize: 18, backgroundColor: 'red', marginTop: 10 }} onPress={() => handleTextClick('Home')}>Home</Text>
+
+            
+              <Text style={{ marginBottom: 10, fontSize: 18 }} onPress={() => handleTextClick('Profile')}>Profile</Text>
+            
             <Text style={{ marginBottom: 10, fontSize: 18 }} onPress={() => handleTextClick('Settings')}>Settings</Text>
+
+            {/*---------------------SIDE MENU WORK SHOULD BE DONE ⬆️⬆️⬆️⬆️⬆️ ---------------------------------- */}
           </View>
         </TouchableWithoutFeedback>
       </Animated.View>
