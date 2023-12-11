b9ce93fcaae9ce87d74ea0eb34cb79d0a891ed51
Aviral <aviral.saxena.11.70@gmail.com>
2023-12-12 00:05:35 +0530

    Revert "Merge branch 'master' of https://github.com/kartikey-mittal/dash-cart"
    This reverts commit 8db3413cadcf3b476abda350e53ba19632e69780, reversing
    changes made to 02a7709d073d91378a8999f3361b75ad261eb49c.
    
    newmssg

--- components/OrderPage/ProgessBar.js
+++ components/OrderPage/ProgessBar.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const ProgressBar = () => {
-  const [step, setStep] = useState(4);
+  const [step, setStep] = useState(2);
 
   const startAnimation = () => {
     setStep(step + 1);
