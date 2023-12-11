ce2bd93214de46c7a72e47903debaee602c1a5bb
kartikey-mittal <g6.kartikey@gmail.com>
2023-12-11 19:45:23 +0530

    checking

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -2,40 +2,17 @@ import React, { useState, useRef } from 'react';
 import { View, Text, TouchableOpacity, Animated, Dimensions, TouchableWithoutFeedback, SafeAreaView } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
-
+import SideMenu from '../components/SideMenu';
 import HomeScreen from './HomeScreen';
 
 const window = Dimensions.get('window');
 
 const TestScreen = () => {
-    const [isOpen, setIsOpen] = useState(true);
-  const animateSideMenu = useRef(new Animated.Value(-window.width)).current;
-
-  const toggleMenu = () => {
-    Animated.timing(animateSideMenu, {
-      toValue: isOpen ? -window.width : 0,
-      duration: 500,
-      useNativeDriver: false,
-    }).start();
-
-    setIsOpen(!isOpen);
-  };
-
+    
     return (
         <SafeAreaView style={{ flex: 1, }}>
             <StatusBar backgroundColor="white" barStyle="light-content" />
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF' }}>
-      <TouchableOpacity onPress={toggleMenu} style={{ backgroundColor: '#4CAF50', padding: 10, borderRadius: 5 }}>
-        <Text style={{ color: '#FFFFFF', fontSize: 16 }}>{isOpen ? 'Close Menu' : 'Open Menu'}</Text>
-      </TouchableOpacity>
-      <TouchableWithoutFeedback onPress={toggleMenu}>
-        <Animated.View style={{ position: 'absolute', left: animateSideMenu, top: 0, bottom: 0, width: window.width * 0.7, backgroundColor: 'yellow', padding: 20 }}>
-          <Text style={{ marginBottom: 10, fontSize: 18 }}>Home</Text>
-          <Text style={{ marginBottom: 10, fontSize: 18 }}>Profile</Text>
-          <Text style={{ marginBottom: 10, fontSize: 18 }}>Settings</Text>
-        </Animated.View>
-      </TouchableWithoutFeedback>
-    </View>
+             <SideMenu/>
 
 
         </SafeAreaView>
