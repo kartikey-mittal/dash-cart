44a68d0f983a5aaca284510c7e71ff6264dac715
Aviral <aviral.saxena.11.70@gmail.com>
2023-12-23 22:12:23 +0530

    cards

--- components/CartPage/CartItemCard.js
+++ components/CartPage/CartItemCard.js
@@ -44,30 +44,53 @@ const CartItemCard = ({ id, title, price, quantity, image }) => {
         borderColor: '#E0E0E0',
         borderWidth: 1,
         opacity: fadeAnim,
+        height:190
       }}
     >
 
-      <View style={{ width: 100, height: 100, alignSelf: 'center', justifyContent: 'center', alignItems: 'center', borderRadius: 8, overflow: 'hidden' }}>
-        <Image source={{ uri: image }} style={{ width: '100%', height: '100%', resizeMode: 'cover' }} />
+      <View style={{ width: 100, height: 100, borderRadius: 8, overflow: 'hidden' }}>
+        <Image source={{ uri: image }} style={{ width: '100%', height: '100%', resizeMode: 'contain' ,position:"absolute",left:-20}} />
       </View>
-      <View style={{ flex: 1, marginLeft: 15 }}>
+      <View style={{ flex: 1, marginLeft:-10 }}>
         <Text style={{ fontSize: 15,  color: '#333',fontFamily:"DMSansSB" }}>{title}</Text>
         <View style={{ flexDirection: 'column', justifyContent: 'center', marginTop: 5 }}>
-          <Text style={{ fontSize: 16, color: '#666',fontFamily:"DMSans" }}>Price: Rs {price}</Text>
-          <Text style={{ fontSize: 17,  marginTop: 10, color: '#333' ,fontFamily:"DMSansSB"}}>Total: Rs {totalPrice}</Text>
+          <Text style={{ fontSize: 16, color: '#666',fontFamily:"DMSans" ,marginTop: 5}}>5 kg</Text>
+          <Text style={{ fontSize: 17,  marginTop: 15, color: '#333' ,fontFamily:"DMSansSB"}}>₹ {totalPrice}</Text>
         </View>
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', marginTop: 10 }}>
-          <TouchableOpacity onPress={handleDecrement} style={{ padding: 5 }}>
-            <MaterialIcons name="remove-circle" size={24} color="#EB8633" />
-          </TouchableOpacity>
-          <Text style={{ fontSize: 18, marginHorizontal: 10 ,fontFamily:"DMSansB"}}>{quantity}</Text>
-          <TouchableOpacity onPress={handleIncrement} style={{ padding: 3 }}>
-            <MaterialIcons name="add-circle" size={24} color="#EB8633" />
-          </TouchableOpacity>
-        </View>
-        <TouchableOpacity onPress={quantity > 1 ? handleRemoveClick : handleRemoveClick} style={{ marginTop: 10 }}>
-        <View style={{ borderRadius: 5, paddingVertical: 6, backgroundColor: '#EB8633', alignItems: 'center' }}>
-          <Text style={{ color: '#FFF', fontSize: 16,fontFamily:"DMSans" }}>{quantity > 1 ? 'REMOVE' : 'DELETE'}</Text>
+              <View style={{ 
+        width:86,
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'flex-end', 
+        marginTop: 25,
+        borderWidth: 1, // Border width
+        borderColor: 'black', // Border color
+        padding: 1, // Padding to give some space inside the border
+        position:"absolute",
+        top:10,
+        right:-10,
+        borderRadius:10,
+              paddingVertical:3
+      }}>
+        <TouchableOpacity onPress={handleDecrement} style={{ padding: 0 }}>
+          <MaterialIcons name="remove" size={20} color="#EB8633" />
+        </TouchableOpacity>
+        <Text style={{ fontSize: 16, marginHorizontal: 10, fontFamily: "DMSansB"}}>{quantity}</Text>
+        <TouchableOpacity onPress={handleIncrement} style={{ padding: 2 }}>
+          <MaterialIcons name="add" size={20} color="#EB8633" />
+        </TouchableOpacity>
+      </View>
+      <Text style={{fontWeight:"600",fontSize:16,position:"absolute",right:115,top:135,width:200}}>Item Total : ₹{totalPrice}</Text>
+      <View style={{
+        borderTopWidth:.4,
+        borderTopColor:"F3EEEA",
+        marginTop:25,
+        width:"100%"
+      }}></View>
+        <TouchableOpacity onPress={quantity > 1 ? handleRemoveClick : handleRemoveClick} style={{ marginTop: 10 ,width:100 ,position:"absolute",right:-5,top:100}}>
+          
+        <View style={{ borderRadius: 5, paddingVertical: 6, backgroundColor: '#EB8633', alignItems: 'center' ,marginTop:20}}>
+          <Text style={{ color: '#FFF', fontSize: 15,fontFamily:"DMSans" }}>{quantity > 1 ? 'REMOVE' : 'DELETE'}</Text>
         </View>
       </TouchableOpacity>
       </View>
--- components/StorePageComponents/ItemCard.js
+++ components/StorePageComponents/ItemCard.js
@@ -36,7 +36,7 @@ const ItemCard = ({ id, title, price, discountPrice, image, weight }) => {
       setQuantity(0);
     }
   };
-
+//
   const handleAddClick = () => {
     Alert.alert('Document ID', `You clicked ADD for item with ID: ${id}`);
     dispatch(addToCart({ id, title, price, quantity: 1, image }));
@@ -44,13 +44,13 @@ const ItemCard = ({ id, title, price, discountPrice, image, weight }) => {
   };
 
   const renderCounterButtons = () => (
-    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', marginTop: 10 }}>
+    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', marginTop: 10 ,borderWidth:1,borderColor:"black",paddingHorizontal:6,paddingVertical:2,borderRadius:10,position:"absolute",top:-40}}>
       <TouchableOpacity onPress={handleDecrement} style={{ padding: 1 }}>
-        <MaterialIcons name="remove-circle" size={22} color="#EB8633" />
+        <MaterialIcons name="remove" size={22} color="#EB8633" />
       </TouchableOpacity>
       <Text style={{ fontSize: 16, marginHorizontal: 5,fontFamily:"DMSansB" }}>{quantity}</Text>
       <TouchableOpacity onPress={handleIncrement} style={{ padding: 1 }}>
-        <MaterialIcons name="add-circle" size={22} color="#EB8633" />
+        <MaterialIcons name="add" size={22} color="#EB8633" />
       </TouchableOpacity>
     </View>
   );
@@ -59,20 +59,20 @@ const ItemCard = ({ id, title, price, discountPrice, image, weight }) => {
     <FontLoader>
     <View style={{ flexDirection: 'row', backgroundColor: 'white', borderRadius: 10, padding: 10, marginHorizontal: 10, marginVertical: 5, borderColor: '#989BA4', borderWidth: 0.5 }}>
        <View style={{ backgroundColor: 'rgba(0, 0, 0, 0)', width: 100, height: 100, alignSelf: 'center', justifyContent: 'center', alignItems: 'center' }}>
-        <Image source={{ uri: image }} style={{ width: '100%', height: '100%', resizeMode: 'contain' }} />
+        <Image source={{ uri: image }} style={{ width: '100%', height: '90%', resizeMode: 'contain' ,borderWidth:2}} />
       </View>
       <View style={{ flex: 1, marginLeft: 25 }}>
         <Text style={{ fontSize: 15, fontWeight: '500',fontFamily:"DMSansSB" }}>{title}</Text>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Text style={{ fontSize: 16, color: 'black', marginTop: 4,fontFamily:"DMSansSB" }}>{price}/-</Text>
-          <Text style={{ fontSize: 13, color: '#a9a9a9', textDecorationLine: 'line-through', marginLeft: 5,fontFamily:"DMSans" }}>{discountPrice}</Text>
+          <Text style={{ fontSize: 16, color: 'black', marginTop: 9,fontFamily:"DMSansSB" }}>{price}/-</Text>
+          <Text style={{ fontSize: 15, color: '#a9a9a9', textDecorationLine: 'line-through',marginTop: 9, marginLeft: 5,fontFamily:"DMSans" }}>{discountPrice}</Text>
         </View>
-        <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 13 }}>
           <Text style={{ fontSize: 16, color: 'black', backgroundColor: '#f2f2f2', paddingHorizontal: 5 ,fontFamily:"DMSans"}}>{weight}</Text>
         </View>
       {quantity === 0 ? (
-        <TouchableOpacity onPress={handleAddClick} style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' }}>
-          <View style={{ borderRadius: 5, padding: 4, alignItems: 'center', justifyContent: 'center', height: 30, borderColor: '#EB8633', borderWidth: 1, width: 60 }}>
+        <TouchableOpacity onPress={handleAddClick} style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end'}}>
+          <View style={{ borderRadius: 5, padding: 4, alignItems: 'center',justifyContent:"center", height: 30, borderColor: '#EB8633', borderWidth: 1, width: 60 }}>
             <Text style={{ color: '#EB8633', fontSize: 14 ,fontFamily:"DMSansB"}}>ADD</Text>
           </View>
         </TouchableOpacity>
